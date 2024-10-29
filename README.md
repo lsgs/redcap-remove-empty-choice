a51a8c27bdb0edd28f7e149382d65e7a2cb462f9
Luke <lukestevens@hotmail.com>
2024-10-29 16:56:27 +1100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# redcap-remove-empty-choice
-REDCap external module for the action tag `@REMOVE-EMPTY-CHOICE` that removes the default, empty option from dropdown list and sql field fields.
+********************************************************************************
+# REDCap External Module: Remove Empty Choice
+
+Luke Stevens, Murdoch Children's Research Institute https://www.mcri.edu.au
+
+[https://github.com/lsgs/redcap-remove-empty-choice](https://github.com/lsgs/redcap-remove-empty-choice)
+********************************************************************************
+
+## Summary
+
+`@REMOVE-EMPTY-CHOICE` action tag for dropdown list and SQL fields that removes the default, empty list option, effectively making the first option the default choice.
+
+Notes:
+* Applies to SQL fields, and is hence useful for automatic selection of a single option returned by the query. 
+* The tag is ignored for all field types except <code>dropdown</code> list <code>sql</code>, or where the \"auto-complete\" option is selected.
---
+++ RemoveEmptyChoice.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * REDCap External Module: Remove Empty Choice
+ * @author Luke Stevens, Murdoch Children's Research Institute
+ */
+namespace MCRI\RemoveEmptyChoice;
+
+use ExternalModules\AbstractExternalModule;
+
+class RemoveEmptyChoice extends AbstractExternalModule
+{
+    const ACTION_TAG = '@REMOVE-EMPTY-CHOICE';
+    protected $isSurvey = false;
+    protected $instrument;
+    protected $taggedFields = [];
+
+    public function redcap_data_entry_form_top($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance) {
+        $this->isSurvey = false;
+        $this->instrument = $instrument;
+        $this->pageTop();
+    }
+
+    public function redcap_survey_page_top($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance) {
+        $this->isSurvey = true;
+        $this->instrument = $instrument;
+        $this->pageTop();
+    }
+
+    protected function pageTop() {
+        // find any tagged dropdown lsit fields on current instrument
+        $this->setTaggedFields();
+        if (!is_array($this->taggedFields) || count($this->taggedFields)===0) return;
+
+        $this->initializeJavascriptModuleObject();
+        ?>
+        <!-- RemoveEmptyChoice: Begin -->
+        <script type="text/javascript">
+            $(function(){
+                var module = <?=$this->getJavascriptModuleObjectName()?>;
+                module.taggedFields = JSON.parse('<?=json_encode($this->taggedFields)?>');
+                module.init = function() {
+                    module.taggedFields.forEach(fldName => {
+                        $('select[name='+fldName+'] option:first').remove();
+                    });
+                };
+                $(document).ready(function(){
+                    module.init();
+                });
+            });
+        </script>
+        <!-- RemoveEmptyChoice: End -->
+        <?php
+    }
+
+    /**
+     * setTaggedFields()
+     * Populate the tagged fields array with names of fields on the current instrument/survey page tagged with the module action tag
+     */
+    public function setTaggedFields() : void {
+        $this->taggedFields = array();
+                
+        $instrumentFields = \REDCap::getDataDictionary('array', false, true, $this->instrument);
+        
+        if ($this->isSurvey && isset($_GET['__page__'])) {
+            global $pageFields;
+            $thisPageFields = array();
+            foreach ($pageFields[$_GET['__page__']] as $pf) {
+                $thisPageFields[$pf] = $instrumentFields[$pf];
+            }
+        } else {
+            $thisPageFields = $instrumentFields;
+        }
+
+        $pattern = "/".static::ACTION_TAG."\b/";
+        foreach ($thisPageFields as $fieldName => $fieldDetails) {
+            if (!in_array($fieldDetails['field_type'], ['dropdown','sql'])) continue;
+            if (!empty($fieldDetails['text_validation_type_or_show_slider_number'])) continue;
+
+            if (preg_match($pattern, $fieldDetails['field_annotation'])) {
+                $this->taggedFields[] = $this->escape($fieldName);
+            }
+        }
+    }
+}
